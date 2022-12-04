4fef5f20ef0e6ed7315f3ecfcd33f1a5a749fa0f
justin kempton <justin@talentsky.com>
2022-12-04 11:03:17 -0800

    basic static site

---
+++ server.js
@@ -0,0 +1,20 @@
+import { Application, send , Router } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+const apiRouter = new Router({ prefix: "/api" });
+apiRouter
+    .get("/", (context) => {
+        context.response.body = { some: "object" };
+    });
+
+app.use(apiRouter.routes());
+
+app.use(async (context) => {
+    await send(context, context.request.url.pathname, {
+      root: `${Deno.cwd()}/static`,
+      index: "index.html",
+    });
+  });
+
+await app.listen({ port: 8000 });
