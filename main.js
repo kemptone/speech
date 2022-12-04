ca5205ff54c93f28b8c4ae99e854284064e9a8a2
justin kempton <justin@talentsky.com>
2022-12-04 15:08:49 -0800

    pulling in examples * changing stuff around

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-import { Application, send , Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, send, Router } from "https://deno.land/x/oak/mod.ts"
 
-const app = new Application();
+const app = new Application()
 
-const apiRouter = new Router({ prefix: "/api" });
+const apiRouter = new Router({ prefix: "/api" })
 apiRouter
-    .get("/", (context) => {
-        context.response.body = { some: "object" };
-    });
+  .get("/", (context) => {
+    context.response.body = { some: "object" }
+  })
 
-app.use(apiRouter.routes());
+app.use(apiRouter.routes())
 
 app.use(async (context) => {
-    await send(context, context.request.url.pathname, {
-      root: `${Deno.cwd()}/static`,
-      index: "index.html",
-    });
-  });
+  await send(context, context.request.url.pathname, {
+    root: `${Deno.cwd()}/static`,
+    index: "index.html",
+  })
+})
 
-await app.listen({ port: 8000 });
+await app.listen({ port: 8000 })
